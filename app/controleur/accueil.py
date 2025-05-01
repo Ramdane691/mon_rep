7dc4bc7c8c8008a0185c418c0c3c310ed7178ece
Ramdane AMAOUZ <ramdane.amaouz@etu.univ-lyon1.fr>
2025-05-01 06:32:32 +0200

    deploiment initial

---
+++ app/controleur/accueil.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+main = Blueprint('accueil', __name__)
+
+@main.route('/')
+def accueil():
+    return render_template('accueil.html', titre ="Bienvenue")
