4bc083a9464c89b823b0bdc5c67b71d907491f72
Ramdane AMAOUZ <ramdane.amaouz@etu.univ-lyon1.fr>
2025-05-02 10:15:29 +0200

    ajout des page de lifapsd bdw et lifapcd, a completer la page de bdw

--- app/controleur/licence2.py
+++ app/controleur/licence2.py
@@ -5,3 +5,31 @@ main = Blueprint('licence2', __name__)
 @main.route('/L2')
 def licence1():
     return render_template('licence2.html', titre ="LicenceInfoLyon1 – LIL1")
+
+@main.route('/L2/lifapsd')
+def lifapsd():
+    return render_template('ue_l2/lifapsd.html', titre ="LicenceInfoLyon1 – LIL1")
+
+@main.route('/L2/lifarchi')
+def lifarchi():
+    return render_template('ue_l2/lifarchi.html', titre ="LicenceInfoLyon1 – LIL1")
+
+@main.route('/L2/stat')
+def stat():
+    return render_template('ue_l2/stat.html', titre ="LicenceInfoLyon1 – LIL1")
+
+@main.route('/L2/lifapcd')
+def lifapcd():
+    return render_template('ue_l2/lifapcd.html', titre ="LicenceInfoLyon1 – LIL1")
+
+@main.route('/L2/bdw')
+def bdw():
+    return render_template('ue_l2/bdw.html', titre ="LicenceInfoLyon1 – LIL1")
+
+@main.route('/L2/pf')
+def pf():
+    return render_template('ue_l2/pf.html', titre ="LicenceInfoLyon1 – LIL1")
+
+@main.route('/L2/lifse')
+def lifse():
+    return render_template('ue_l2/lifse.html', titre ="LicenceInfoLyon1 – LIL1")
---
+++ app/static/script/l1/lifapi.js
@@ -0,0 +1,19 @@
+//la syntaxe
+
+
+// les variables et les types
+
+
+
+
+// permiers pas 
+
+// fonction et procedure
+
+//boucle if / else
+
+// boucle for
+
+// boucle while
+
+// tableau
